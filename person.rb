a7ca84827dc85de79fb18b7c4fd315d742d06e0d
Robert Di Ninno <robertdininno@gmail.com>
2022-06-27 20:50:14 +0000

    add person class and initializer

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  @@number_of_players = 0;
+
+  def initialize 
+    @@number_of_players += 1
+    @name = "Player #{@@number_of_players}"
+    @score = 3
+  end
+
+  def loosePoint
+    @score -= 1
+    "#{@name} #{@score}/3"
+  end
+end
+
+p robert = Person.new
+p thomas = Person.new
+
+p robert.loosePoint
+p robert
