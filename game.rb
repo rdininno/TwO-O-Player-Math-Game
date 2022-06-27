138d62e0b3bfa6142d3cd848679a76b0220f9df3
Robert Di Ninno <robertdininno@gmail.com>
2022-06-27 23:13:58 +0000

    comment change

--- game.rb
+++ game.rb
@@ -7,20 +7,18 @@ class Game
   def start(players)
     # set current player
     currentPlayer = players[0]
-    # create question
     question = Question.new
 
     puts "------New Turn------"
     p "#{currentPlayer.name}: #{question.display}"
     print ">"
+
     # player input
     playerAnswer = gets.chomp.to_i
 
-    # call helpers to check the user input against answer and update score
+    # call in the helpers!
     updateScore(currentPlayer, playerAnswer, question.answer)
-    # display current score
     currentScore(currentPlayer, players)
-    # check score to see if game is over
     checkScore(currentPlayer, players)
   end
 end
