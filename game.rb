78aed6d63059431832fbc87d6ab6819b81e421a2
Robert Di Ninno <robertdininno@gmail.com>
2022-06-27 22:25:52 +0000

    add logic to check score for winner and update score

--- game.rb
+++ game.rb
@@ -3,6 +3,9 @@ require "./question"
 
 class Game
   @@game_over = false
+  def currentScore(currentPlayer, players)
+    puts "#{currentPlayer.name}: #{currentPlayer.displayScore} vs #{players[1].name}: #{players[1].displayScore}"
+  end
 
   def start(players)
     currentPlayer = players[0]
@@ -13,19 +16,32 @@ class Game
     print ">"
     playerAnswer = gets.chomp.to_i
 
-    if (playerAnswer == question.answer)
-      puts "congrats! that was the correct answer!" 
+    updateScore(currentPlayer, playerAnswer, question.answer, players)
+  end
+
+  def updateScore(currentPlayer, playerAnswer, answer, players)
+    if(playerAnswer == answer)
+      puts "congrats! that was the correct answer!"
+      checkScore(currentPlayer, players)
     else
       puts "Nope, not that!"
+      currentPlayer.loosePoint
+      currentScore(currentPlayer, players)
+      checkScore(currentPlayer, players)
+    end
+  end
+
+  def checkScore(currentPlayer, players)
+    if currentPlayer.score < 1
+      puts "-----------------"
+      puts "dang #{currentPlayer.name} that's it. You've lost."
+      puts "#{players[1].name} wins with a score of #{players[1].displayScore}"
+      puts "Game over man, Game over"
+      puts "-----------------"
+      return 
+    else 
       players = players.reverse()
       start(players)
-    end  
+    end
   end
 end
-
-player1 = Person.new
-player2 = Person.new
-
-players = [player1, player2]
-game = Game.new
-game.start(players)
