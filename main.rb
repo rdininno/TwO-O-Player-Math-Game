b457e6991043eab73c5d348df0a1528d6ff1d587
Robert Di Ninno <robertdininno@gmail.com>
2022-06-27 22:29:57 +0000

    add require game, create new game and start it up

--- main.rb
+++ main.rb
@@ -1,11 +1,14 @@
 require "./person"
-require "./question"
+require "./game"
 
 class Main
   player1 = Person.new
   player2 = Person.new
 
   players = [player1, player2]
+
+  game = Game.new
+  game.start(players)
   
 end
 
